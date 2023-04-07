255a7f89a1837068a6d2e228aa8174b6128cb94f
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-04-07 18:18:54 +0300

    Module #3, task #4 completed primitive variable types tasks

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c, h, a, r;
+        c = 99;
+        h = 104;
+        a = 97;
+        r = 114;
     }
 }
